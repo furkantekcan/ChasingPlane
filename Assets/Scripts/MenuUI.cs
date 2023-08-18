92dc5e15e2c31f35489f6339c27194ebbcc887bc
furkantekcan <furkanrftekcan@gmail.com>
2023-08-18 16:32:10 +0300

    Initial prototype

---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+    public Button playButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener(OnPlayButtonClicked);
+    }
+
+    public void OnPlayButtonClicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SelectPlane(int planeIndex)
+    {
+        PlayerPrefs.SetInt("planeIndex", planeIndex);
+    }
+}
---
+++ Assets/Scripts/MissileBehaviour.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileBehaviour : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public Transform target;
+    public GameObject dust;
+    public GameObject explosionEffect;
+    public float speed = 6f, rotationSpeed = 120f, dustWait = .05f;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(MakingDust());
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = transform.up * speed;
+        if (target != null)
+        {
+            Vector2 direction = (Vector2)target.position - rb.position;
+            direction.Normalize();
+            float angle = Vector3.Cross(direction, transform.up).z;
+            rb.angularVelocity = -rotationSpeed * angle;
+        }
+    }
+
+    IEnumerator MakingDust()
+    {
+        while (gameObject)
+        {
+            yield return new WaitForSeconds(dustWait);
+            GameObject dustTemp = Instantiate(dust, transform.position, dust.transform.rotation);
+            Destroy(dustTemp, 4f);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        string tag = other.gameObject.tag;
+        if (tag.Equals("plane"))
+        {
+            BlowUpPlane(other.gameObject.transform);
+        }
+        if (tag.Equals("missile"))
+        {
+            BlowUpSelf();
+        }
+    }
+
+    void BlowUpPlane(Transform plane)
+    {
+        BlowUpSelf();
+        plane.parent.GetComponent<PlaneBehaviour>().GameOver(transform);
+    }
+
+    void BlowUpSelf()
+    {
+        GameObject tempExplosion = Instantiate(explosionEffect, transform.position, dust.transform.rotation);
+        Destroy(tempExplosion, 1.2f);
+        Destroy(gameObject);
+    }
+
+}
